7bf23df484ca484cedbc96464b9d082ecbd03ab4
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-03-09 01:06:11 -0800

    Added questions to ask users

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ Packages
 
 Tutorials
 * [SVG Polygon](https://www.w3schools.com/graphics/svg_polygon.asp)
-
+* [More inquirer](https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts)
 
 
 [Return back to Title](#free-logo-idea)
--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 const inquirer = require('inquirer');
+const shapeClass = require('./lib/shape.js');
 
+questions = [
+    {
+        type: 'input',
+        message: 'Create a initials. (Up to three characters and no ".")',
+        name: 'initial',
+        default: "NJS"
+    },
+    {
+        type: 'input',
+        message: 'Enter the text color. You can use a keyword (i.e blue) or hexadecimal number.',
+        name: 'textColor',
+        default: "purple"
+    },
+    {
+        type: 'list',
+        message: 'What shape do you do want for your logo? Use arrow key Up and Down to navigate the list of choices. Press enter when you have decided. ',
+        name: 'shape',
+        choices: ["Triangle", "Circle", "Square"],
+        default: "Circle",
+    },
+    {
+        type: 'input',
+        message: 'Enter the logo color. You can use a keyword (i.e blue) or hexadecimal number.',
+        name: 'shapeColor',
+        default: "DFFFFD"
+    },
+];
+
+function askUsers(){
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(`It worked! Check answers`);
+        })
+        .catch((error) =>{
+            if (error.isTtyError){
+                console.log("Prompt couldn't be rendered in the current environment");
+            } else {
+                console.log("How?");
+            }
+    });
+}
+
+askUsers();
